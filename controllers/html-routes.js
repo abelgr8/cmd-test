beac7ec95a15f3d98908e0517d2f48579cd66492
Asif Kabir <Asif6152000@Asifs-MacBook-Air.local>
2018-04-27 12:35:33 -0400

    commit before pulling from master

--- controllers/html-routes.js
+++ controllers/html-routes.js
@@ -3,8 +3,13 @@ var router = express.Router();
 var path = require('path');
 
 router.get('/', function (req, res) {
-	res.send('<h1>It works!</h1>');
+	res.sendFile(path.join(__dirname,'../public/ind.html'));
 });
 
-module.exports = router;
+router.get('/signup', function (req, res) {
+	console.log('hey');
+	res.sendFile(path.join(__dirname, '../public/signup.html' )); 
+} ); 
+
+module.exports = router; 
 
