93db15749adc5f12508c8ff50bf1eda8fcdad275
justin <justin@tcadmins-MacBook-Pro-59.local>
2018-04-27 11:48:08 -0400

    Passport and views

--- server.js
+++ server.js
@@ -5,6 +5,12 @@ var PORT = process.env.PORT || 3000;
 var express = require('express');
 var app = express();
 
+// Load Passport
+var passport = require('passport');
+LocalStrategy = require('passport-local').Strategy;
+
+var env = require('dotenv').load();
+
 //Load Handlebars
 var exphbs = require('express-handlebars');
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
@@ -14,9 +20,19 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json());
 
+// For Passport
+app.use(session({ secret: 'dunkirk', resave: true, saveUninitialized: true })); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.get('/', function (req, res) {
+    res.send('Welcome to Passport with Sequelize');
+});
+
 //Routing
 var htmlRouter = require('./controllers/html-routes.js');
 var apiRouter = require('./controllers/api-routes.js');
+var authRoute = require('./controllers/auth.js')(app, passport);
+// Be sure to create auth.js file in the controller folder
 
 //Load Sequelize
 var db = require("./models");
@@ -28,6 +44,10 @@ app.use(express.static('public'));
 
 app.use('/',htmlRouter);
 app.use('/api',apiRouter);
+
+//load passport strategy
+require('.config/passport/passport.js')(passport, models.user);
+// Be sure to add passport.js file and folder
 
 
 db.sequelize.sync({force:true}).then(function() {
